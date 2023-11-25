9b617e36acb987645b61a9006449021d1c402043
Rishab Chaudhary <rishab2245@gmail.com>
2023-11-25 15:58:48 +0530

    .

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -1,15 +1,17 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './SideBarcss.css';
 import ProjectButtonBind from './ProjectButtonBind'
 import MemberButtonbind from './MemberButtonBind';
 import AddProject from './AddProject';
 
-const SideBar = () => {
+const SideBar = ({profunc}) => {
+
+
   return (
     <div className='side'>
       <ProjectButtonBind/>
       <MemberButtonbind/>
-      <AddProject/>
+      <AddProject profunc={profunc}/>
     </div>
   )
 }
