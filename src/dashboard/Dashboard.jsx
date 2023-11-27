312b45039b330f4b2afd4cb164dbf3f2b5caa23f
Dhruv Sharma <dsd71213@gmail.com>
2023-11-27 18:37:56 +0530

    UI changes

--- src/dashboard/Dashboard.jsx
+++ src/dashboard/Dashboard.jsx
@@ -6,27 +6,47 @@ import MainSection from '../components/MainSection'
 import { useLocation } from 'react-router'
 import React, { useEffect, useState } from 'react'
 import AddProject from '../AddProj/AddProject'
+import axios from 'axios'
 
 const Dashboard = () => {
-  let [add,setadd]=useState(false)
-  const loacation = useLocation();
-  let[projedata,setprojedata] = useState({Project:"frontend",discription:"hellow world"});
-  console.log(loacation.state.email);
+  let [add, setadd] = useState(false)
+  const location = useLocation();
+  let [projedata, setprojedata] = useState({ Project: "frontend", discription: "hellow world" });
+  console.log(location.state.auth);
+  const auth = location.state.auth
 
-  const profunc = ()=>{
+  const profunc = () => {
     setadd(!add);
     console.log("add")
   }
+  const GetBoards = async () => {
+    try {
+      const response = await axios.get('https://teammanagement.onrender.com/api/board/getBoards/', {
+        headers: {
+          withCredentials: true,
+          'Authorization':auth
+        }
+      })
+      console.log(response)
+    }
+    catch (error) {
+      console.error(error);
+    }
+  }
+  useEffect(() => {
+    GetBoards();
+  }, [])
+
   return (
     <>
       <Header />
       <div className='below'>
-        <SideBar profunc={profunc} projectdata={projedata}/>
+        <SideBar profunc={profunc} projectdata={projedata} />
         {
-          add ? <MainSection/> : <AddProject profunc={profunc} setprojectdata={setprojedata}/>
+          add ? <MainSection /> : <AddProject profunc={profunc} setprojectdata={setprojedata} />
         }
       </div>
-      </>
+    </>
   )
 }
 export default Dashboard
