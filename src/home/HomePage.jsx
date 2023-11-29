bb15d5bb01e4ed64532841839d99c576e69f47d5
Tanishka Agarwal <agarwaltani642004@gmail.com>
2023-11-30 01:46:06 +0530

    homePage

--- src/home/HomePage.jsx
+++ src/home/HomePage.jsx
@@ -28,18 +28,18 @@ function Home() {
            </div>
            <div className="wave">
            <svg xmlns="http://www.w3.org/2000/svg" width="100%" viewBox="0 0 1512 169" fill="none">
-  <path d="M401.354 32.818C271.066 36.6527 77.7019 117.361 -0.199241 170.008L1500.57 197.699C1523.29 132.061 1554.88 0.862285 1499.48 1.16896C1430.23 1.5523 1229.95 118.442 1013.42 119.641C796.898 120.839 564.216 28.0247 401.354 32.818Z" fill="url(#paint0_linear_542_43)" stroke="url(#paint1_linear_542_43)" stroke-width="1.18632"/>
-  <defs>
-    <linearGradient id="paint0_linear_542_43" x1="24.3324" y1="108.483" x2="1530.75" y2="100.144" gradientUnits="userSpaceOnUse">
-      <stop stop-color="#D9D9D9"/>
-      <stop offset="1" stop-color="#D9D9D9" stop-opacity="0.69"/>
-    </linearGradient>
-    <linearGradient id="paint1_linear_542_43" x1="-0.199092" y1="170.009" x2="1493.76" y2="170.784" gradientUnits="userSpaceOnUse">
-      <stop stop-color="white"/>
-      <stop offset="1" stop-color="#707070" stop-opacity="0"/>
-    </linearGradient>
-  </defs>
-</svg>
+                 <path d="M401.354 32.818C271.066 36.6527 77.7019 117.361 -0.199241 170.008L1500.57 197.699C1523.29 132.061 1554.88 0.862285 1499.48 1.16896C1430.23 1.5523 1229.95 118.442 1013.42 119.641C796.898 120.839 564.216 28.0247 401.354 32.818Z" fill="url(#paint0_linear_542_43)" stroke="url(#paint1_linear_542_43)" stroke-width="1.18632"/>
+                 <defs>
+                 <linearGradient id="paint0_linear_542_43" x1="24.3324" y1="108.483" x2="1530.75" y2="100.144" gradientUnits="userSpaceOnUse">
+  <stop stop-color="#D9D9D9"/>
+  <stop offset="1" stop-color="#D9D9D9" stop-opacity="0.69"/>
+</linearGradient>
+<linearGradient id="paint1_linear_542_43" x1="-0.199092" y1="170.009" x2="1493.76" y2="170.784" gradientUnits="userSpaceOnUse">
+  <stop stop-color="white"/>
+  <stop offset="1" stop-color="#707070" stop-opacity="0"/>
+</linearGradient>
+            </defs>
+        </svg>
 
            </div>
             </div></>
