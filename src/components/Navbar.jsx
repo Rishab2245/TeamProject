a64617174d1da4ed8655116420a3ab46aa4ff8ad
Dhruv Sharma <dsd71213@gmail.com>
2023-12-03 21:12:05 +0530

    Fix

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,36 +3,39 @@ import Tabs from "./Tabs"
 import Todo from "../../TaskSection/Todo";
 //import ProgressReport from '../progress/Progress';
 
-const Navbar = ({auth , tododata}) => {
-    let [type,settype] = useState("tasks")
-    console.log(tododata)
+const Navbar = ({ auth, tododata }) => {
+  let [type, settype] = useState("tasks")
+  console.log(tododata)
   return (
-    <>
-        <div className='bar' style={{height:"10%", width:"100%"}}>
-        <Tabs type={type} settype={settype}/>
+    <div>
+      <div className='bar' style={{ height: "10%", width: "100%" }}>
+        <Tabs type={type} settype={settype} />
       </div>
       {type === "chat" && (
-        <div style={{height:"80%", width:"100%"}}>
-         <h1>chats</h1>
+        <div style={{ height: "80%", width: "100%" }}>
+          <h1>chats</h1>
         </div>
       )}
       {type === "tasks" && (
-        <div style={{height:"80%", width:"100%"}}>
+        <div style={{ height: "80%", width: "100%" }}>
 
-        <Todo auth={auth} tododata={tododata}/>
+          <Todo auth={auth} tododata={tododata} />
         </div>
       )}
       {type === "progress" && (
 
         <div>
-         
 
-        <div style={{height:"80%", width:"100%"}}>
 
-          <h1>progress</h1>
+          <div style={{ height: "80%", width: "100%" }}>
+
+            <h1>progress</h1>
+          </div>
+
         </div>
       )}
-    </>
+    </div>
+
   )
 }
 
